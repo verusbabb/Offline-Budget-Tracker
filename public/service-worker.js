f68ba63b626930e5d143b2fc1f8df9f9a8e25250
Steve Babb <steve.babb@outlook.com>
2021-04-12 12:36:17 -0500

    update s-w with activate

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,42 @@
+const DATA_CACHE_NAME = "data-cache-v1";
+
 self.addEventListener("install", (event) => {
-  console.log("Inside the UPDATED 4 install handler:", event);
+  //   pre-cache transaction data
+  event.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
+
+  //   pre-cache static assets
+  event.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./icons/icon-192x192.png",
+        "./icons/icon-512x512.png",
+      ]);
+    })
+  );
+  console.log("Install");
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("Inside the activate handler:", event);
+// activate
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== "static" && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener(fetch, (event) => {
